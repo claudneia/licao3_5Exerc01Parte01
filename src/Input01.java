63c4f4cddaed428e46805aeecca5a93ccdb0543d
claudneia <164636272+claudneia@users.noreply.github.com>
2024-06-11 21:43:46 -0300

    Add files via upload
    lição 3.5 Exercício01 Parte01

---
+++ src/Input01.java
@@ -0,0 +1,29 @@
+
+import javax.swing.JOptionPane;
+
+public class Input01 {
+    public static void main(String[] args) {
+       String input = JOptionPane.showInputDialog(null,"insira uma resposta");
+       int intVar1 = Integer.parseInt(input);
+       System.out.println(intVar1+1);
+       
+       int intVar2 = Integer.parseInt("Insira uma resposta: ");
+       
+       
+       
+        //Create a JOptionPane.
+        //Store the input as a String and print it.
+        
+        
+        
+        //Parse the input as an int.
+        //Print its value +1
+        
+        
+        
+        //Try creating a dialog, parsing it, and initializing an int in a single line.
+        //You should have only one semicolon (;) in this line.
+
+        
+    }
+}
